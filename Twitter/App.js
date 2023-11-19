407a813aff78f750c94d16dbc800ffe79957c4d2
superfast678 <superdanhvip@gmail.com>
2023-11-19 22:21:18 +0700

    Format folder, add lib, navigation

--- Twitter/App.js
+++ Twitter/App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Tab.Navigator
+        initialRouteName="notification"
+        screenOptions={{
+          tabBarStyle: {
+            height: 65,
+            backgroundColor: "rgb(0,0,0)",
+            borderTopWidth: 1,
+            borderTopColor:""
+          },
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "white",
+          headerStyle:{
+            backgroundColor:"black",
+            borderBottomColor:""
+          }
+        }}
+      >
+        
+      </Tab.Navigator>
+   </NavigationContainer>
   );
 }
 
